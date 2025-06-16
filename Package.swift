95d742a740907eb1e855dec8ed8387d7db1539f2
uzair.shahzad <uzair.shahzad@mycomera.com>
2025-06-16 19:33:39 +0400

    Updated.

--- Package.swift
+++ Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyWhooshSampleFramework",
+    name: "MyWhooshSampleFrameWork",
     platforms: [.iOS(.v15)], // or whatever minimum you support
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyWhooshSampleFramework",
-            targets: ["MyWhooshSampleFramework"]),
+            name: "MyWhooshSampleFrameWork",
+            targets: ["MyWhooshSampleFrameWork"]),
     ],
     targets: [
-        .binaryTarget(name: "MyWhooshSampleFramework", url: "https://github.com/uzair/BinaryFrameWork/releases/download/1.0.2/MyWhooshSampleFrameWork.xcframework.zip", checksum: "043895dd142c203e7e1472aa738b4606eb1b0afb101ed4380954d5d16ccb1a7b")
+        .binaryTarget(name: "MyWhooshSampleFrameWork", url: "https://github.com/uzair/BinaryFrameWork/releases/download/1.0.2/MyWhooshSampleFrameWork.xcframework.zip", checksum: "043895dd142c203e7e1472aa738b4606eb1b0afb101ed4380954d5d16ccb1a7b")
         
 
     ]
