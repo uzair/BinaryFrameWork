54a5705278dbd933b2e824eb5c5b59ca553c81cc
uzair.shahzad <uzair.shahzad@mycomera.com>
2025-06-17 17:15:38 +0400

    Added some classes back.

---
+++ MyWhooshSampleFrameWork/Classes/Sample1.swift
@@ -0,0 +1,24 @@
+//
+//  Sample1.swift
+//  MyWhooshSampleFrameWork
+//
+//  Created by Uzair Shahzad on 16/06/2025.
+//
+
+
+private class Sample1 {
+    private init() {}
+    
+    private func sayHello(name: String) -> String {
+        return "Hello, \(name) 👋"
+    }
+}
+
+
+private class Sample2 {
+    private init() {}
+    
+    private func sayHello(name: String) -> String {
+        return "Hello, \(name) 👋"
+    }
+}
