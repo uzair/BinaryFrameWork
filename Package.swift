a3c50ffd94e154a156fd71e4a78003bd21cdf31f
uzair.shahzad <uzair.shahzad@mycomera.com>
2025-06-17 12:55:48 +0400

    Release 1.0.6

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "MyWhooshSampleFrameWork",
-    platforms: [.iOS(.v15)], // or whatever minimum you support
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyWhooshSampleFrameWork",
             targets: ["MyWhooshSampleFrameWork"]),
     ],
     targets: [
-        .binaryTarget(name: "MyWhooshSampleFrameWork", url: "https://github.com/uzair/BinaryFrameWork/releases/download/1.0.5/MyWhooshSampleFrameWork.xcframework.zip", checksum: "9226ed63938d8af2b85d19c8b7ca93ea164c80ce2aa5deb755c36a7b9ee874cd")
-        
+        .binaryTarget(
+            name: "MyWhooshSampleFrameWork",
+            url: "https://github.com/uzair/BinaryFrameWork/releases/download/1.0.6/MyWhooshSampleFrameWork.xcframework.zip",
+            checksum: ""
+        )
     ]
 )
