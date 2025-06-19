6f64d59e8aa5d886e5192cd7cc55d6b26fa5a010
uzair.shahzad <uzair.shahzad@mycomera.com>
2025-06-19 13:29:17 +0400

    Release 2.0.12

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyWhooshSampleFrameWork",
-            url: "https://github.com/uzair/BinaryFrameWork/releases/download/2.0.11/MyWhooshSampleFrameWork.xcframework.zip",
-            checksum: "6dd9f910474c9898c41eb9f57cd0d1635de4f71684a84edaafc0f1d89f71c4ba"
+            url: "https://github.com/uzair/BinaryFrameWork/releases/download/2.0.12/MyWhooshSampleFrameWork.xcframework.zip",
+            checksum: "41ea75f0547fe7f3582cba158b43b035934614dabd707a157e3967ce97d0f259"
         )
     ]
 )
